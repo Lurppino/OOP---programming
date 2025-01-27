1868b6cb126ada3c0eef065170f0632a4168bcac
Lurppino <luka.ulmanen@gmail.com>
2025-01-27 12:31:50 +0200

    Tehtävä 3.4 - Nuoli-luokan viimeistely

--- C# - Olio/ArrowVersion-1.cs
+++ C# - Olio/ArrowVersion-1.cs
@@ -28,6 +28,20 @@
                 ShaftLength = shaftLenght;
             }
 
+            public static Nuoli CreateEliteArrow()
+            {
+                return new Nuoli(Tip.Diamond, Fletching.EagleFeather, 100);
+            }
+
+            public static Nuoli CreateBeginnerArrow()
+            {
+                return new Nuoli(Tip.Wood, Fletching.Leaf, 70);
+            }
+
+            public static Nuoli CreateBasicArrow()
+            {
+                return new Nuoli(Tip.Steel, Fletching.ChickenFeather, 100);
+            }
             public double PalautaHinta()
             {
                 double Price = 0;
@@ -65,33 +79,74 @@
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Albert's Arrow's! How may I help you today?");
+            Console.WriteLine("Do you want to: (1) Buy a predefined arrow or (2) Order a custom arrow?");
+            int choice = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Choose an arrowhead type: (1) Wood, (2) Steel, (3) Diamond");
-            int selectedTip = int.Parse(Console.ReadLine());
-            if (selectedTip < 1 || selectedTip > 3)
+            Nuoli arrow = null;
+
+            if (choice == 1)
             {
-                Console.WriteLine("Invalid arrowhead type. Please select 1, 2 or 3.");
+                Console.WriteLine("Choose a predefined arrow type:");
+                Console.WriteLine("1. Elite Arrow (Diamond arrowhead, Eagle feather fletching, 100cm shaft length.");
+                Console.WriteLine("2. Basic Arrow (Steel arrowhead, Chicken feather fletching, 80cm shaft length.");
+                Console.WriteLine("3. Beginner Arrow (Wood arrowhead, Leaf fletching, 75cm shaft length");
+
+                int arrowChoice = int.Parse(Console.ReadLine());
+
+                if (arrowChoice == 1)
+                {
+                    arrow = Nuoli.CreateEliteArrow();
+                }
+                else if (arrowChoice == 2)
+                {
+                    arrow = Nuoli.CreateBasicArrow();
+                }
+                else if (arrowChoice == 3)
+                {
+                    arrow = Nuoli.CreateBeginnerArrow();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    return;
+                }
+            }
+
+            else if (choice == 2)
+            {
+                Console.WriteLine("Choose an arrowhead type: (1) Wood, (2) Steel, (3) Diamond");
+                int selectedTip = int.Parse(Console.ReadLine());
+                if (selectedTip < 1 || selectedTip > 3)
+                {
+                    Console.WriteLine("Invalid arrowhead type. Please select 1, 2 or 3.");
+                    return;
+                }
+                Tip arrowhead = (Tip)selectedTip;
+
+                Console.WriteLine("Choose the fletching type: (1) Leaf, (2) Chicken feather, (3) Eagle feather");
+                int selectedFletching = int.Parse(Console.ReadLine());
+                if (selectedFletching < 1 || selectedFletching > 3)
+                {
+                    Console.WriteLine("Invalid fletching type. Please select 1, 2 or 3.");
+                    return;
+                }
+                Fletching fletching = (Fletching)selectedFletching;
+
+                Console.WriteLine("Choose the shaft length: 60-100cm");
+                int selectedLength = int.Parse(Console.ReadLine());
+                if (selectedLength < 60 || selectedLength > 100)
+                {
+                    Console.WriteLine("Invalid shaft length. Please select a value between 60-100cm");
+                    return;
+                }
+                arrow = new Nuoli(arrowhead, fletching, selectedLength);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
                 return;
             }
-            Tip arrowhead = (Tip)selectedTip;
-
-            Console.WriteLine("Choose the fletching type: (1) Leaf, (2) Chicken feather, (3) Eagle feather");
-            int selectedFletching = int.Parse(Console.ReadLine());
-            if (selectedFletching < 1 || selectedFletching > 3)
-            {
-                Console.WriteLine("Invalid fletching type. Please select 1, 2 or 3.");
-                return;
-            }
-            Fletching fletching = (Fletching)selectedFletching;
-
-            Console.WriteLine("Choose the shaft length: 60-100cm");
-            int selectedLength = int.Parse(Console.ReadLine());
-            if (selectedLength < 60 || selectedLength > 100)
-            {
-                Console.WriteLine("Invalid shaft length. Please select a value between 60-100cm");
-                return;
-            }
-            Nuoli arrow = new Nuoli(arrowhead, fletching, selectedLength);
 
             Console.WriteLine($"Arrowhead: {arrow.Arrowhead}");
             Console.WriteLine($"Fletching: {arrow.FletchingType}");
