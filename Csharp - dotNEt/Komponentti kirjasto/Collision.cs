b6ea26c033006332b64a155a233a5a243e817b81
Lurppino <luka.ulmanen@gmail.com>
2025-08-14 14:10:05 +0300

    added component library in github

---
+++ Csharp - dotNEt/Komponentti kirjasto/Collision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukaLib
+{
+    public class Collision2d
+    {
+        public float Radius;
+
+        public Collision2d(float radius)
+        {
+            Radius = radius;
+        }
+
+        public static bool CheckCollision2d(Transform2d tA, Collision2d cA, Transform2d tB, Collision2d cB)
+        {
+            return Raylib_cs.Raylib.CheckCollisionCircles(tA.Position, cA.Radius, tB.Position, cB.Radius);
+        }
+    }
+}
