ede1fa7cb636a4502fd460edbc3d51fe122e3d68
Lurppino <luka.ulmanen@gmail.com>
2025-09-02 09:46:46 +0300

    begin valikkopeli

---
+++ Csharp - dotNEt/Valikkopeli/Valikkopeli/Game.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valikkopeli
+{
+    public class Game
+    {
+        private float x = 100;
+        private float y = 100;
+        private float dx = 3;
+        private float dy = 3;
+
+        public void UpdateGame()
+        {
+            x += dx;
+            y += dy;
+
+            if (x < 0 || x > 800) dx *= -1;
+            if (y < 0 || y > 600) dy *= -1;
+        }
+
+        public void DrawGame()
+        {
+            Raylib.DrawText("Peli kaynnissa!", 10, 10, 20, Color.Green);
+            Raylib.DrawCircle((int)x, (int)y, 20, Color.Red);
+        }
+    }
+}
---
+++ Csharp - dotNEt/Valikkopeli/Valikkopeli/Program.cs
@@ -0,0 +1,28 @@
+﻿using Raylib_cs;
+using System;
+using Valikkopeli;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Raylib.InitWindow(800, 600, "Valikkopeli");
+        Raylib.SetTargetFPS(60);
+
+        Game peli = new Game();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            peli.UpdateGame();
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            peli.DrawGame();
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
